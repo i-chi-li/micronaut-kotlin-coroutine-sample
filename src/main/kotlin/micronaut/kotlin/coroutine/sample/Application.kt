7081942d3d1f451b968c77247f4b6d312579dac4
i-chi-li <i-chi-li@i-chi-li.info>
2020-03-21 07:04:51 +0900

    以下のコマンドで生成したプロジェクト mn create-app --lang=kotlin micronaut-kotlin-coroutine-sample

---
+++ src/main/kotlin/micronaut/kotlin/coroutine/sample/Application.kt
@@ -0,0 +1,14 @@
+package micronaut.kotlin.coroutine.sample
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("micronaut.kotlin.coroutine.sample")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
